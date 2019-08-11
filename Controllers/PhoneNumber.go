75d60cdb216ce017574a106008126c5c3c9f90bd
Ludovic Gueth <ludovicgueth@gmail.com>
2019-08-11 10:18:50 +0200

    add verification on PhoneNumber

--- Controllers/PhoneNumber.go
+++ Controllers/PhoneNumber.go
@@ -3,14 +3,26 @@ package Controllers
 import (
 	"api/ApiHelpers"
 	"api/Models"
+
 	"github.com/gin-gonic/gin"
 )
-
 
 func AddNewPhoneNumber(c *gin.Context) {
 	var phoneNumber Models.PhoneNumber
 	c.BindJSON(&phoneNumber)
 	err := Models.AddNewPhoneNumberToContact(&phoneNumber)
+	if phoneNumber.Number == "" {
+		ApiHelpers.RespondJSON(c, 404, phoneNumber)
+		return
+	}
+	if phoneNumber.Name == "" {
+		ApiHelpers.RespondJSON(c, 404, phoneNumber)
+		return
+	}
+	if phoneNumber.ContactId == 0 {
+		ApiHelpers.RespondJSON(c, 404, phoneNumber)
+		return
+	}
 	if err != nil {
 		ApiHelpers.RespondJSON(c, 404, phoneNumber)
 	} else {
